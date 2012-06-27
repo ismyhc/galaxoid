8495ab9d12ffcf5ef6151389c9e282e63bd4a580
Jacob Davis <ismyhc@gmail.com>
2012-06-27 19:25:01 -0400

    stuff..

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
 source 'http://rubygems.org'
 
-gem 'chingu', "0.8.1"
-gem 'rest-client'
-gem 'crack', require: 'crack/xml'
+gem 'chingu', "0.9rc8"
+gem 'rest-client', "1.6.7"
+gem 'crack', "0.3.1"
 
 group :development do
   gem 'ocra', '1.3.0'
--- lib/main.rb
+++ lib/main.rb
@@ -1,5 +1,6 @@
 require 'rubygems' rescue nil
 require 'chingu'
+
 require_relative "play"
 require_relative "player"
 require_relative "enemy"
@@ -9,23 +10,17 @@ require_relative "score_bonus"
 require_relative "start_menu"
 require_relative "high_score"
 require_relative "enter_name"
-=begin
-require File.expand_path "../play.rb", __FILE__
-require File.expand_path "../player.rb", __FILE__
-require File.expand_path "../enemy.rb", __FILE__
-require File.expand_path "../bullet.rb", __FILE__
-require File.expand_path "../life_bonus.rb", __FILE__
-require File.expand_path "../score_bonus.rb", __FILE__
-require File.expand_path "../start_menu.rb", __FILE__
-require File.expand_path "../high_score.rb", __FILE__
-require File.expand_path "../enter_name.rb", __FILE__
-=end
+
 include Gosu
 include Chingu
 
 class Main < Chingu::Window
   def initialize
     super(800, 600, false)
+    Gosu::Image.autoload_dirs << File.expand_path("../images", __FILE__)
+    Gosu::Sound.autoload_dirs << File.expand_path("../sounds", __FILE__)
+    Gosu::Song.autoload_dirs << File.expand_path("../songs", __FILE__)
+    Gosu::Font.autoload_dirs << File.expand_path("../fonts", __FILE__)
     retrofy
     push_game_state(StartMenu)
   end
